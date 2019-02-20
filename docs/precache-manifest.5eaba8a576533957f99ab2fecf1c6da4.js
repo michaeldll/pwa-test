0c055ca9d4bb8a0e31901c97815d6bfb44ba4484
michael <mo>
2019-02-20 14:31:15 +0100

    add docs

---
+++ docs/precache-manifest.5eaba8a576533957f99ab2fecf1c6da4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "014b0eda0287d4090bff",
+    "url": "/static/css/main.e7ffb6e1.chunk.css"
+  },
+  {
+    "revision": "014b0eda0287d4090bff",
+    "url": "/static/js/main.014b0eda.chunk.js"
+  },
+  {
+    "revision": "29b4a3c4ecc2b092d106",
+    "url": "/static/js/1.29b4a3c4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a7016df2e05db578032553b3d6d46219",
+    "url": "/index.html"
+  }
+];
